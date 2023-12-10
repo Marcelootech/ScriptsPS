57176c82479fd578d3b293c8d301fb654af1f8c6
Marcelootech <153430704+Marcelootech@users.noreply.github.com>
2023-12-10 08:34:17 -0300

    Add files via upload

---
+++ Wshell.ps1
@@ -0,0 +1,10 @@
+﻿$wshell = New-Object -com WScript.Shell
+
+$wshell | Get-Member
+
+$wshell.Popup("Esse Curso é muito legal")
+
+$wshell.run("Notepad")
+$wshell.AppActivate("Notepad")
+Start-Sleep 2
+$wshell.SendKeys("Esse Curso é muito legal")
